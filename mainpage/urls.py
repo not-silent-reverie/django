e01bec90d51062ddea5a303f393b965c73bb69cd
silentreverie <silentreverie@mail.ru>
2026-06-26 11:31:34 +0300

    changes

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('feedback/', views.feedback_view, name='feedback'),
+    path('feedback/success/', views.feedback_success, name='feedback_success'),
+    path('vk-webhook/', views.vk_webhook, name='vk_webhook'),  # <-- Добавьте эту строку
 ]
